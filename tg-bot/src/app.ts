a04a8b0af91afd94f83f2e628fa9a287cc93447a
uzarixx <andrei.213241@gmail.com>
2023-01-27 13:59:23 +0200

    add-test-scene-in-bot

--- tg-bot/src/app.ts
+++ tg-bot/src/app.ts
@@ -1,27 +1,37 @@
-import { ConfigService } from './config/config.service';
-import { IConfigService } from './config/config.interface';
-import { Telegraf } from 'telegraf';
-import { IBotContext } from './context/context.interface';
-import { Command } from './commands/command.class';
-import { StartCommand } from './commands/start.command';
+import {ConfigService} from './config/config.service';
+import {IConfigService} from './config/config.interface';
+import {Scenes, Telegraf} from 'telegraf';
+import {IBotContext, ISceneContext} from './context/context.interface';
+import {Command} from './commands/command.class';
+import {StartCommand} from './commands/start.command';
 import LocalSession from 'telegraf-session-local';
 
+const {enter} = Scenes.Stage
+const greeterScene = new Scenes.BaseScene<ISceneContext>("greeter");
+greeterScene.enter(ctx => ctx.reply("Hi"));
+greeterScene.leave(ctx => ctx.reply("Bye"));
+greeterScene.hears("hi", enter<ISceneContext>("greeter"));
+greeterScene.on("message", ctx => ctx.replyWithMarkdownV2("Send `hi`"));
+
 class Bot {
-  bot: Telegraf<IBotContext>;
-  commands: Command[] = [];
+    bot: Telegraf<IBotContext>;
+    commands: Command[] = [];
 
-  constructor(private readonly configService: IConfigService) {
-    this.bot = new Telegraf<IBotContext>(this.configService.get('TOKEN'));
-    this.bot.use(new LocalSession({ database: 'sessions.json' }).middleware());
-  }
+    constructor(private readonly configService: IConfigService) {
 
-  init() {
-    this.commands = [new StartCommand(this.bot)];
-    for (const command of this.commands) {
-      command.handle();
+        this.bot = new Telegraf<IBotContext>(this.configService.get('TOKEN'));
+        this.bot.use(new LocalSession({database: 'sessions.json'}).middleware());
+        this.bot.use(new Scenes.Stage<any>([greeterScene], {ttl: 10}))
     }
-    this.bot.launch();
-  }
+
+    init() {
+        this.commands = [new StartCommand(this.bot)];
+        for (const command of this.commands) {
+            command.handle();
+        }
+        this.bot.command('test', ctx => ctx.scene.enter('greeter'))
+        this.bot.launch();
+    }
 
 }
 
--- tg-bot/src/context/context.interface.ts
+++ tg-bot/src/context/context.interface.ts
@@ -1,4 +1,4 @@
-import { Context } from 'telegraf';
+import { Context, Scenes } from 'telegraf';
 
 export interface SessionData {
   courseLike: boolean;
@@ -6,4 +6,10 @@ export interface SessionData {
 
 export interface IBotContext extends Context {
   session: SessionData;
+  scene: Scenes.SceneContextScene<ISceneContext>;
+}
+
+export interface ISceneContext extends Context {
+  myContextProp: string;
+  scene: Scenes.SceneContextScene<ISceneContext>;
 }
